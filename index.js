351a71642e786d5e85aa2bdf1579961eca43d8c4
Watthanai Chotcheewasunthorn <watthanai.chot@aware.co.th>
2019-09-03 16:47:42 +0700

    fix mock data

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var revenueRoute = require("./app/router/revenueRoute")
 var memberRoute = require("./app/router/memberRoute")
 app.use(express.urlencoded())
 app.use(express.json())
+app.use((req, res, next) => {
+  res.setHeader("Content-Type", "application/json")
+  next()
+})
+
 app.use('/revenue', revenueRoute)
 app.use('/member', memberRoute)
 var PORT = process.env.PORT || 3000
